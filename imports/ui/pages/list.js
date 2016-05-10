3af1606ceac4a6a46c622c46866a19a6768398a7
Paul Worrall <paul.worrall@interition.net>
2016-05-10 13:13:10 +0100

    BON-298: Consolidated adding members and acknowledgement onto the list view

--- imports/ui/pages/list.js
+++ imports/ui/pages/list.js
@@ -2,21 +2,32 @@
  * Created by pjworrall on 03/05/2016.
  */
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './list.html';
 
+Template.list.onCreated(function () {
+
+    // Keystore and Zone will be available as Template.instance().X
+    var ks = this.KeyStore = ZidStore.get();
+
+    var web3;
+    if (typeof ks != 'undefined') {
+        web3 = ZonafideWeb3.getInstance(ZonafideEnvironment.Node, ks);
+        this.Zone = web3.eth.contract(ZonafideEnvironment.abi);
+    } else {
+        // todo: alert problem with settign Web3 provider
+        console.log('whoops! All fell apart on create because of no key store');
+    }
+
+});
 
 Template.list.helpers({
 
     zads() {
-        // todo: I've got big GO ZADS'
-      console.log('providing us with current ZADS');
-
         return ZidUserLocalData.find();
     }
 });
-
-
 
 Template.list.events({
 
@@ -26,22 +37,14 @@ Template.list.events({
 
         console.log('performing create event');
 
-        var keyStore = ZidStore.get();
+        var Zone = Template.instance().Zone;
+        var KeyStore = Template.instance().KeyStore;
 
-        var web3;
-        if (typeof keyStore != 'undefined') {
-            web3 = ZonafideWeb3.getInstance(ZonafideEnvironment.Node,keyStore);
-        } else {
-            // todo: alert problem with settign Web3 provider
-            console.log('performing create event');
-        }
-
-        var Zone = web3.eth.contract(ZonafideEnvironment.abi);
-
-        Zone.new(ZonafideEnvironment.caller(keyStore.getAddresses()[0]), function (error, contract) {
+        Zone.new(ZonafideEnvironment.caller(KeyStore.getAddresses()[0]),
+            function (error, contract) {
                 if (!error) {
                     if (typeof contract.address != 'undefined') {
-                       console.log('Confirmed. address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
+                        console.log('Confirmed. address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
 
                         ZidUserLocalData.insert({_id: contract.address});
 
@@ -49,10 +52,73 @@ Template.list.events({
                     }
                 } else {
                     // todo: alert, geth call failed
-                console.log('geth callback error: ' + error);
+                    console.log('geth callback error: ' + error);
                 }
 
             });
+    },
 
+    'submit .addMember'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        console.log('addMember.event: called');
+
+        const zad = event.target.zad.value;
+        const zid = event.target.zid.value;
+
+        var Zone = Template.instance().Zone;
+        var KeyStore = Template.instance().KeyStore;
+
+        var zone = Zone.at(zad);
+
+        // todo: quorum set hard at 1 for now
+        const quorum = 1;
+
+        zone.setMembers([zid], quorum,
+            ZonafideEnvironment.caller(KeyStore.getAddresses()[0]),
+
+            function (error, obj) {
+                if (error) {
+                    console.log("ERROR - members.events: " + err);
+                    Session.set('result', "something unexpected happened, sorry!");
+                } else {
+                    console.log("INFO - members.events: " + obj);
+                }
+            });
+    },
+
+    'submit .acknowledge'(event) {
+
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        console.log('acknowledge.events: called');
+
+        const zad = event.target.zad.value;
+
+        var Zone = Template.instance().Zone;
+        var KeyStore = Template.instance().KeyStore;
+
+        var zone = Zone.at(zad);
+
+        zone.setAcknowledgement(
+            ZonafideEnvironment.caller(KeyStore.getAddresses()[0]),
+            function (err, obj) {
+
+                if (err) {
+                    console.log("ERROR - acknowledge.events: " + err);
+                    Session.set('result', "something unexpected happened, sorry!");
+                } else {
+                    console.log("INFO - acknowledge.events: " + obj);
+                    Template.instance().result.set(obj);
+                }
+
+            });
     }
+
 });
+
+
+
+
