3f97e818460186a42931cb17b1c529985c1c5a5d
Paul Worrall <paul.worrall@interition.net>
2016-05-20 12:48:47 +0100

    menu close on item click works

--- imports/ui/pages/menu.js
+++ imports/ui/pages/menu.js
@@ -7,13 +7,12 @@ import { Session } from 'meteor/session';
 import './menu.html';
 
 Template.menu.onRendered(function () {
-    //$('.tooltipped').tooltip();
-
-    console.log("setting up navbar close on click...");
+    $('.tooltipped').tooltip();
 
     // to make bootstrap menu close when menu item chosen
-    $(document).on('click','.navbar-collapse.in',function(e) {
-        if( $(e.target).is('a') ) {
+    $(document).on('click','.navbar-collapse',function(e) {
+        if( $(e.target).is('i') ) {
+
             $(this).collapse('hide');
         }
     });
