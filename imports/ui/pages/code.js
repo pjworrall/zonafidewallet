299c3e862a2bd1c564105cc02773d4ce76533a0a
Paul Worrall <paul.worrall@interition.net>
2016-09-23 13:18:52 +0100

    added extra Zone Address share for Recipients

--- imports/ui/pages/code.js
+++ imports/ui/pages/code.js
@@ -15,6 +15,8 @@ Template.code.onRendered(function () {
         text: Template.instance().data.address
     });
 
+    $('.tooltipped').tooltip();
+
 });
 
 Template.code.events({
@@ -39,20 +41,51 @@ Template.code.events({
         }
 
     },
-    'submit .share'(event) {
+
+    // todo: these will all go in a service component....eventually...
+
+    'submit .shareWithAcknowledger'(event) {
 
         // Prevent default browser form submit
         event.preventDefault();
 
         const address = event.target.address.value;
 
-        // todo: this will all go in a service component....eventually...
-
         // this is the complete list of currently supported params you can pass to the plugin (all optional)
         var options = {
             message: 'Secure a Zone for me. Please Acknowledge ' + address, // not supported on some apps (Facebook, Instagram)
-            subject: 'My Zonafide ID', // fi. for email
+            subject: 'Zone Address for my Activity', // fi. for email
             //files: ['', ''], // an array of filenames either locally or remotely
+            //todo: this should be to the explanation page for verifiers
+            url: 'https://www.zonafide.net',
+            chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+        };
+
+        var onSuccess = function(result) {
+            console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+            console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+        };
+
+        var onError = function(msg) {
+            console.log("Sharing failed with message: " + msg);
+        };
+
+        window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+
+    },
+    'submit .shareWithRecipient'(event) {
+
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        const address = event.target.address.value;
+
+        // this is the complete list of currently supported params you can pass to the plugin (all optional)
+        var options = {
+            message: 'Here is the Zone Address for my planned activity: ' + address, // not supported on some apps (Facebook, Instagram)
+            subject: 'Zone Address for my Activity', // fi. for email
+            //files: ['', ''], // an array of filenames either locally or remotely
+            //todo: this should be to the explanation page for verifiers
             url: 'https://www.zonafide.net',
             chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
         };
