03f4f5afdae4606126dd16048a2a383367c50216
Paul Worrall <paul.worrall@interition.net>
2016-09-23 12:41:45 +0100

    added social sharing to ZID and ZAD qr code pages

--- imports/ui/pages/code.js
+++ imports/ui/pages/code.js
@@ -15,7 +15,7 @@ Template.code.onRendered(function () {
         text: Template.instance().data.address
     });
 
-})
+});
 
 Template.code.events({
 
@@ -37,6 +37,36 @@ Template.code.events({
             Router.go("list");
 
         }
+
+    },
+    'submit .share'(event) {
+
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        const address = event.target.address.value;
+
+        // todo: this will all go in a service component....eventually...
+
+        // this is the complete list of currently supported params you can pass to the plugin (all optional)
+        var options = {
+            message: 'Secure a Zone for me. Please Acknowledge ' + address, // not supported on some apps (Facebook, Instagram)
+            subject: 'My Zonafide ID', // fi. for email
+            //files: ['', ''], // an array of filenames either locally or remotely
+            url: 'https://www.zonafide.net',
+            chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+        };
+
+        var onSuccess = function(result) {
+            console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+            console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+        };
+
+        var onError = function(msg) {
+            console.log("Sharing failed with message: " + msg);
+        };
+
+        window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
 
     }
 });
--- imports/ui/pages/identities.js
+++ imports/ui/pages/identities.js
@@ -108,7 +108,36 @@ Template.identities.events({
 
         });
 
+    },
+    'submit .share'(event) {
+
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // todo: this will all go in a service component....eventually...
+
+        // this is the complete list of currently supported params you can pass to the plugin (all optional)
+        var options = {
+            message: 'Get in the Zone with me. My Zonafide ID is ' + Session.get('zid'), // not supported on some apps (Facebook, Instagram)
+            subject: 'My Zonafide ID', // fi. for email
+            //files: ['', ''], // an array of filenames either locally or remotely
+            url: 'https://www.zonafide.net',
+            chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+        };
+
+        var onSuccess = function(result) {
+            console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+            console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+        };
+
+        var onError = function(msg) {
+            console.log("Sharing failed with message: " + msg);
+        };
+
+        window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+
     }
+
 
 });
 
