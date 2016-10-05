3469284e575493ac152cf02d3f4fe374ba92d7c8
Paul Worrall <paul.worrall@interition.net>
2016-10-05 17:43:45 +0100

    hard coded working code into setting to transfer Eth to another account

--- imports/ui/pages/settings.js
+++ imports/ui/pages/settings.js
@@ -6,6 +6,8 @@ import {ReactiveVar} from 'meteor/reactive-var';
 
 import lightwallet from 'eth-lightwallet';
 
+import Transaction from 'ethereumjs-tx';
+
 import './settings.html';
 
 Template.settings.onRendered(function () {
@@ -91,6 +93,92 @@ Template.settings.events({
         lightwallet.keystore.deriveKeyFromPassword(password, function (err, pwDerivedKey) {
             alert('Your seed is: "' + ZidStore.get().getSeed(pwDerivedKey) + '". Please write it down.')
         })
+    },
+
+    'submit .transferEth'() {
+
+        event.preventDefault();
+
+        var password = prompt('Provide a session Password', 'Password');
+
+        lightwallet.keystore.deriveKeyFromPassword(password, function (err, pwDerivedKey) {
+
+            if (err) {
+                sAlert.error('Failed to derive key from password..dev issue', {timeout: 'none'});
+            } else {
+
+                // todo: problem - hooked web3 provider nonce conflict arises here. hw3p keeps track of nonce values as well.
+
+                var w3 = ZonafideWeb3.getInstance();
+
+                var address = ZidStore.get().getAddresses()[0];
+
+                var count = w3.eth.getTransactionCount(address);
+
+                // need to get these from the form
+                // todo: should really identify a framework for field validation
+                var recipient = '0xf76216c08976e36aa276580efa818ffc9235cefa';
+                var amount =  '0x' + '1000000000000';
+
+                // todo: got to get a solution for managing the gas properties across the app
+                var txData = {
+                    "nonce": count,
+                    "gasLimit" : "0x2fefd8",
+                    "gasPrice" : "0xba43b7400",
+                    "to": recipient,
+                    "value": amount,
+                };
+
+                console.log("txData: " + JSON.stringify(txData));
+
+                var tx = new Transaction(txData);
+
+                console.log("tx: " + JSON.stringify(tx));
+
+                var rawTx = tx.serialize().toString('hex');
+
+                var signedTx = lightwallet.signing.signTx(ZidStore.get(),
+                    pwDerivedKey,
+                    rawTx,
+                    address
+                );
+
+                w3.eth.sendRawTransaction(signedTx, function (txError, tranHash) {
+                    if (txError) {
+                        sAlert.info("Network reported: " + txError,
+                            {timeout: 'none', sAlertIcon: 'fa fa-info-circle', sAlertTitle: 'Transaction Failed'});
+
+                    } else {
+
+                        sAlert.info('A request to transfer ' + amount + ' has been made: ' + tranHash,
+                            {timeout: 'none', sAlertIcon: 'fa fa-info-circle', sAlertTitle: 'Transfer request made'});
+
+                        ZoneTransactionReceipt.check(tranHash, ZonafideWeb3.getInstance(), function (rctError, receipt) {
+                            if (rctError) {
+                                sAlert.info('Could not complete transaction: ' + rctError.toString(),
+                                    {
+                                        timeout: 'none',
+                                        sAlertIcon: 'fa fa-info-circle',
+                                        sAlertTitle: 'Transfer failed'
+                                    });
+                            } else {
+                                sAlert.info('Transfer recorded at blocknumber ' + receipt.blockNumber,
+                                    {
+                                        timeout: 'none',
+                                        sAlertIcon: 'fa fa-info-circle',
+                                        sAlertTitle: 'Transfer completed'
+                                    });
+                            }
+                        });
+
+                    }
+                });
+
+            }
+
+        });
+
+
     }
 
 
