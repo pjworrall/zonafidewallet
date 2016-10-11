bf1b330ed44d7f6fa0d4471767ad86e1ac4f2ce8
Paul Worrall <paul.worrall@interition.net>
2016-10-11 19:53:17 +0100

    prefer front camera set to false, to default to back camera

--- imports/startup/client/qrscanner.js
+++ imports/startup/client/qrscanner.js
@@ -23,7 +23,7 @@ ZoneQRScanner = (function () {
                         callback(error,false);
                     },
                     {
-                        "preferFrontCamera" : true, // iOS and Android
+                        "preferFrontCamera" : false, // iOS and Android
                         "showFlipCameraButton" : true, // iOS and Android
                         "prompt" : "Place a barcode inside the scan area", // supported on Android only
                         "formats" : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
