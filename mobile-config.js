c0a509cfcf7a39fc2984729f8338d4bbeffd5014
Paul Worrall <paul.worrall@interition.net>
2016-10-11 19:21:48 +0100

    got mobile config working. mostly dev environment settings (eg WebStorm Scopes for Babel) that enable the config to be used

--- mobile-config.js
+++ mobile-config.js
@@ -2,45 +2,49 @@
  * Created by pjworrall on 25/07/2016.
  */
 
-// App.info({
-//     id: 'net.interition.zonafide',
-//     name: 'Zonafide',
-//     description: 'Zonafide: work together to stop fraud',
-//     author: 'Paul Worrall, Interition Limited',
-//     email: 'info@zonafide.net',
-//     website: 'http://zonafide.net',
-//     version: '0.0.8'
-// });
+App.info({
+    id: 'net.interition.zonafide',
+    name: 'Zonafide',
+    description: 'Zonafide: work together to stop fraud',
+    author: 'Paul Worrall, Interition Limited',
+    email: 'info@zonafide.net',
+    website: 'http://zonafide.net',
+    version: '0.1.1'
+});
 
-// App.icons({
-//     // iOS
-//     'iphone_2x': 'resources/icons/icon-60x60@2x.png',
-//     'ipad': 'resources/icons/icon-76x76.png',
-//     'ipad_2x': 'resources/icons/icon-76x76@2x.png',
-//
-//     // Android
-//     'android_mdpi': 'resources/icons/icon-48x48.png',
-//     'android_hdpi': 'resources/icons/icon-72x72.png',
-//     'android_xhdpi': 'resources/icons/icon-96x96.png'
-// });
+App.icons({
+    // iOS
+    // 'iphone_2x': 'resources/icons/icon-60x60@2x.png',
+    // 'ipad': 'resources/icons/icon-76x76.png',
+    // 'ipad_2x': 'resources/icons/icon-76x76@2x.png',
 
-// App.launchScreens({
-//     // iOS
-//     // 'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-//     // 'iphone5': 'resources/splash/splash-320x568@2x.png',
-//     // 'ipad_portrait': 'resources/splash/splash-768x1024.png',
-//     // 'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-//     // 'ipad_landscape': 'resources/splash/splash-1024x768.png',
-//     // 'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
-//
-//     // Android
-//     'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-//     'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-//     'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-//     'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-//     'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-//     'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
-// });
+    // Android
+    'android_mdpi': 'resources/icons/icon-48x48.png',
+    'android_hdpi': 'resources/icons/icon-72x72.png',
+    'android_xhdpi': 'resources/icons/icon-96x96.png',
+    'android_xxhdpi': 'resources/icons/icon-144x144.png',
+    'android_xxxhdpi': 'resources/icons/icon-192x192.png'
+});
+
+App.launchScreens({
+    // iOS
+    // 'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+    // 'iphone5': 'resources/splash/splash-320x568@2x.png',
+    // 'ipad_portrait': 'resources/splash/splash-768x1024.png',
+    // 'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+    // 'ipad_landscape': 'resources/splash/splash-1024x768.png',
+    // 'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
+
+    // Android
+    'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+    'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+    'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+    'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+    'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+    'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+    // 'android_xxhdpi_portrait': 'resources/splash/splash-1080x1440.png',
+    // 'android_xxhdpi_landscape': 'resources/splash/splash-1440x1080.png'
+});
 
 // App.setPreference('StatusBarOverlaysWebView', 'false');
 // App.setPreference('StatusBarBackgroundColor', '#000000');
