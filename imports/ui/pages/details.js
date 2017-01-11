be5b55803fb836ba80a1d1083b71c1e5b069034a
Paul Worrall <paul.worrall@interition.net>
2017-01-11 17:15:02 +0000

    added extra details to Acitivity page lie Aknoweldger, Acknowledgers, Verifier..

--- imports/ui/pages/details.js
+++ imports/ui/pages/details.js
@@ -2,10 +2,10 @@
  * Created by pjworrall on 21/05/2016.
  */
 
-import { Template } from 'meteor/templating';
+import {Template} from 'meteor/templating';
 //import { QRCode } from 'meteor/steeve:jquery-qrcode';
 import  {ZidUserLocalData, ZidStore} from '/imports/startup/client/globals.js';
-import  { ZonafideWeb3 } from '/imports/startup/client/web3.js';
+import  {ZonafideWeb3} from '/imports/startup/client/web3.js';
 
 import './details.html';
 
@@ -36,10 +36,17 @@ Template.details.onCreated(function () {
         from: ZidStore.get().getAddresses()[0]
     });
 
-    // todo: this should be a privileged method call if that is possible
-    //this.serviceProvider = this.zone.serviceProvider;
-    this.serviceProvider = false;
+    this.members = this.zone.getMembers({
+        from: ZidStore.get().getAddresses()[0]
+    });
 
+    this.acknowledgers = this.zone.getAcknowledgers({
+        from: ZidStore.get().getAddresses()[0]
+    });
+
+    this.verifier = this.zone.getVerifier({
+        from: ZidStore.get().getAddresses()[0]
+    });
 
 });
 
@@ -48,7 +55,7 @@ Template.details.onRendered(function () {
     $('#qrcode').qrcode({
         render: 'div',
         size: 400,
-        text: Template.instance().data.address
+        text: Template.instance().address
     });
 
 });
@@ -74,26 +81,54 @@ Template.details.helpers({
         return Template.instance().confirmed;
     },
 
-    serviceProvider() {
-        return Template.instance().serviceProvider;
+    // todo: this is currently only providing one member for MVP
+    members() {
+
+        if (Template.instance().members && Template.instance().members.length >= 1) {
+            return Template.instance().members[0];
+        } else {
+            return "None";
+        }
+
+    },
+
+    acknowledgers() {
+
+        if (Template.instance().acknowledgers && Template.instance().acknowledgers.length >= 1) {
+            return Template.instance().acknowledgers[0];
+        } else {
+            return "None";
+        }
+
+    },
+
+    verifier() {
+
+        if (Template.instance().verifier) {
+            return Template.instance().verifier;
+        } else {
+            return "N/A";
+        }
+
     }
 
 });
 
 Template.details.events({
 
-    'click .js-delete'(event,template) {
+    'click .js-delete'(event, template) {
 
         // Prevent default browser form submit
         event.preventDefault();
 
         if (confirm('Are you sure?')) {
 
-            console.log("removing: " + Template.instance().data.address );
+            console.log("removing: " + Template.instance().address);
 
             //todo: small chance the zone might not exist but decided not to test because it is so unlikely
+            //todo: maybe should be data._id not we have it?
 
-            ZidUserLocalData.remove({address: template.data.address});
+            ZidUserLocalData.remove({address: template.address});
 
             Router.go("list");
 
@@ -102,14 +137,14 @@ Template.details.events({
     },
 
 
-    'click .js-share'(event,template) {
+    'click .js-share'(event, template) {
 
         // Prevent default browser form submit
         event.preventDefault();
 
         // this is the complete list of currently supported params you can pass to the plugin (all optional)
         var options = {
-            message: 'Secure a Zone for me. Please Acknowledge ' + template.data.address, // not supported on some apps (Facebook, Instagram)
+            message: 'Secure a Zone for me. Please Acknowledge ' + template.address, // not supported on some apps (Facebook, Instagram)
             subject: 'Zone Address for my Activity', // fi. for email
             //files: ['', ''], // an array of filenames either locally or remotely
             //todo: this should be to the explanation page for verifiers
@@ -117,26 +152,26 @@ Template.details.events({
             chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
         };
 
-        var onSuccess = function(result) {
+        var onSuccess = function (result) {
             console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
             console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
         };
 
-        var onError = function(msg) {
+        var onError = function (msg) {
             console.log("Sharing failed with message: " + msg);
         };
 
         window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
 
     },
-    'click .js-send'(event,template) {
+    'click .js-send'(event, template) {
 
         // Prevent default browser form submit
         event.preventDefault();
 
         // this is the complete list of currently supported params you can pass to the plugin (all optional)
         let options = {
-            message: 'Here is the Zone Address for my planned activity: ' + template.data.address, // not supported on some apps (Facebook, Instagram)
+            message: 'Here is the Zone Address for my planned activity: ' + template.address, // not supported on some apps (Facebook, Instagram)
             subject: 'Zone Address for my Activity', // fi. for email
             //files: ['', ''], // an array of filenames either locally or remotely
             //todo: this should be to the explanation page for verifiers
@@ -144,12 +179,12 @@ Template.details.events({
             chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
         };
 
-        let onSuccess = function(result) {
+        let onSuccess = function (result) {
             console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
             console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
         };
 
-        let onError = function(msg) {
+        let onError = function (msg) {
             console.log("Sharing failed with message: " + msg);
         };
 
