5acd414b5e3ba7f9c495d1e927c368df61adefe7
Paul Worrall <paul.worrall@interition.net>
2017-02-26 08:04:31 +0000

    introduced monitor skeleton for watching contract events and the start of its test

--- imports/startup/client/monitor.js
+++ imports/startup/client/monitor.js
@@ -6,21 +6,62 @@
  *
  */
 
-// import  {ZidUserLocalData} from '/imports/startup/client/globals.js';
-
 /*
  *
- * let events = zone.allEvents({fromBlock: 0, toBlock: 'latest'});
- * events.watch(dispatcher);
+ * Skeleton of a monitor that will watch for contract Events
+ *
+ * Should replace receipt polling and form the hub of Activity
+ * event watching and dispatch across the App.
+ *
+ * Example use:
+ *     let watch = ZonafideMonitor.getInstance();
+ *     watch.monitor(Activity_or_Zone);
  *
  */
 
 
-export class ZonafideMonitor {
+let ZonafideMonitor = (function ZonafideMonitor() {
 
-    watch (zone) {
-        let events = zone.allEvents({fromBlock: 0, toBlock: 'latest'});
-        events.watch( () => { console.log("ooh big poo!!!!!!!!") });
+    let _monitor;
+
+    function Monitor() {
+
+        let _events;
+
+        return {
+
+            test: "test",
+            monitor: function (zone) {
+                console.log("start watching");
+                _events = zone.allEvents({fromBlock: 0, toBlock: 'latest'});
+                _events.watch(function (error,result) {
+                    console.log(">>>>>>>>>>>>>>>>>>>>  event fired: " + result);
+                });
+            },
+
+            stopMonitoring: function () {
+                console.log("stop watching");
+                _events.stopWatching();
+            }
+        }
     }
 
-}
+    return {
+        getInstance: function () {
+
+            if (_monitor) {
+                return _monitor;
+            } else {
+                _monitor = new Monitor();
+                return _monitor ;
+            }
+        }
+    }
+
+}());
+
+
+
+
+
+export {ZonafideMonitor};
---
+++ imports/startup/client/monitor.test.js
@@ -0,0 +1,32 @@
+/**
+ * Created by pjworrall on 25/02/2017.
+ */
+
+// todo: need to figure out how to mock a contract/Zone/Activity
+
+import {ZonafideMonitor} from './monitor.js';
+
+
+describe('monitor', function () {
+
+    it('should be a function', function (done) {
+
+        let _monitor = ZonafideMonitor.getInstance();
+
+       chai.assert(typeof _monitor.monitor === 'function', "typeof did not report monitor was a function");
+
+       done();
+
+    });
+
+    it('should report test attribute value as test', function (done) {
+
+        let _monitor = ZonafideMonitor.getInstance();
+
+        chai.assert.equal(_monitor.test, "test", "was not the value [test]");
+
+        done();
+
+    });
+
+});
